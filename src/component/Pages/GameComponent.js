e51ad0946c183afe5fa7eb6b090c5ce9234699ed
Benreefgit config --global core.editor code <ben@outrun.io>
2023-06-27 15:30:40 +0930

    Team creation and displaying functioning

--- src/component/Pages/GameComponent.js
+++ src/component/Pages/GameComponent.js
@@ -1,49 +1,86 @@
-// import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
-// const GameComponent = () => {
-//   const [gameId, setGameId] = useState('');
-//   const [score, setScore] = useState(0);
-//   const [intercepts, setIntercepts] = useState([]);
-//   const [centerPasses, setCenterPasses] = useState([]);
-//   const [shootingStats, setShootingStats] = useState([]);
+function GamePage() {
+  const [teams, setTeams] = useState([]);
+  const [selectedTeam, setSelectedTeam] = useState('');
+  const [selectedOpposition, setSelectedOpposition] = useState('');
+  const history = useHistory();
 
-//   const handleSaveGame = () => {
-//     const data = {
-//       gameId: gameId,
-//       score: score,
-//       intercepts: intercepts,
-//       centerPasses: centerPasses,
-//       shootingStats: shootingStats,
-//     };
+  useEffect(() => {
+    fetchTeams();
+  }, []);
 
-//     fetch('/games/finish', {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json',
-//       },
-//       body: JSON.stringify(data),
-//     })
-//       .then((response) => {
-//         if (response.ok) {
-//           console.log('Game saved successfully');
- 
-//         } else {
-//           console.error('Failed to save game:', response.status);
+  const fetchTeams = () => {
+    fetch('/api/teams')
+      .then((res) => res.json())
+      .then((data) => setTeams(data))
+      .catch((error) => console.error('Error fetching teams:', error));
+  };
 
-//         }
-//       })
-//       .catch((error) => {
-//         console.error('Failed to save game:', error);
-//       });
-//   };
+  const handleTeamChange = (event) => {
+    setSelectedTeam(event.target.value);
+  };
 
+  const handleOppositionChange = (event) => {
+    setSelectedOpposition(event.target.value);
+  };
 
-//   return (
-//     <div>
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
-//       <button onClick={handleSaveGame}>Save/Finish Game</button>
-//     </div>
-//   );
-// };
+    // Create a new game in the database
+    const gameData = {
+      teamId: selectedTeam,
+      oppositionId: selectedOpposition === 'opposition' ? null : selectedOpposition,
+    };
 
-// export default GameComponent;
+    fetch('/api/games', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(gameData),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        const gameId = data.gameId;
+        // Redirect to the scoring page with the game ID
+        history.push(`/scoring/${gameId}`);
+      })
+      .catch((error) => console.error('Error creating game:', error));
+  };
+
+  return (
+    <div>
+      <h1>Start Game</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Select Your Team:</label>
+          <select value={selectedTeam} onChange={handleTeamChange}>
+            <option value="">Choose a team</option>
+            {teams.map((team) => (
+              <option key={team.team_id} value={team.team_id}>
+                {team.team_name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <label>Select Opposition:</label>
+          <select value={selectedOpposition} onChange={handleOppositionChange}>
+            <option value="opposition">Opposition</option>
+            {teams.map((team) => (
+              <option key={team.team_id} value={team.team_id}>
+                {team.team_name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <button type="submit">Start Game</button>
+      </form>
+    </div>
+  );
+}
+
+export default GamePage;
--- src/component/Pages/TeamList.js
+++ src/component/Pages/TeamList.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import TeamListDisplay from './TeamListDisplay';
+
 // const TeamList = () => {
 //   const [teams, setTeams] = useState([]);
 
@@ -40,23 +40,34 @@ import TeamListDisplay from './TeamListDisplay';
 // export default TeamList;
 
 
-
 function TeamList() {
   const [teams, setTeams] = useState([]);
 
-  function findAll() {
+  useEffect(() => {
     fetch('/api/teams')
       .then(res => res.json())
-      .then(res => setTeams(res))
-  }
-
-  useEffect(findAll, [])
+      .then(data => setTeams(data))
+  }, []);
 
   return (
-    <TeamListDisplay 
-      teams={teams}
-    />
-  )
+    <div>
+      <h2>Teams</h2>
+      {teams.map((team, index) => (
+        <div key={index}>
+          <h3>{team.team_name}</h3>
+          <p>Coach: {team.coach_name}</p>
+          <p>Players:</p>
+          <ul>
+            {team.players.map((player, index) => (
+              <li key={index}>
+                {player.player_name} - {player.preferred_position}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default TeamList;
